063900718797c9efac4eef19e2fdbfa50958d560
GregMarkitan <s26387@pjwstk.edu.pl>
2026-06-19 14:46:14 +0200

    Created fields and construcor for Student.java

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student { 
+	private String id;
+	private String fullName;
+	private String groupName;
+	private int loyaltyPoints;
+
+	public Student(String id, String fullName, String groupName, int loyaltyPoints) {
+		this.id = id;
+		this.fullName = fullName;
+		this.groupName = groupName;
+		this.loyaltyPoints = loyaltyPoints;
+	}
+}

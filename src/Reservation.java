20e45e4f3915c7480aceb42fe6e74aafc31e8c27
GregMarkitan <s26387@pjwstk.edu.pl>
2026-06-19 15:50:09 +0200

    Created fields and constructor for reservation.java

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,6 +1,21 @@
 public class Reservation implements Displayable {
-    public double calculateTotalCost(DiscountPolicy discountPolicy) {
+	private String id;
+	private Student student;
+	private Equipment equipment;
+	private int days;
+	private ReservationStatus status;
+
+	public Reservation(String id, Student student, Equipment equipment, int days) {
+		this.id = id;
+		this.student = student;
+		this.equipment = equipment;
+		this.days = days;
+		this.status = ReservationStatus.ACTIVE;
+	}
+    
+
+	public double calculateTotalCost(DiscountPolicy discountPolicy) {
         double priceBeforeDiscount = equipment.calculateDailyPrice() * days;
         return discountPolicy.applyDiscount(student, priceBeforeDiscount);
-    }
+	}
 }
